ea8e7779d123637c821e7e76d12eae1e568b79fa
Amanda Hausmann <anhausma@uci.edu>
2024-07-14 19:21:33 -0700

    buttons for countries + adding a country, new fonts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,18 +9,29 @@ function App() {
   return (
     <div className="dashboard">
       <header className="dashboard-header">
+
         <p className="user-travels">
             Sphynx's Travels
         </p>
+        <button className="add-travels-button">+</button>
+
       </header>
+
       <body className="dashboard-body">
+
       <img src={mercatorImage} alt="Map" className="mercator-pic" />
-      <h2 className="countries-title"> Countries </h2>
-      <ul className="country-list">
+      
+      <div className="countries-title-container">
+        <h2 className="countries-title"> Countries </h2>
+      </div>
+      <ul className="countries-list">
         {countries.map((country, index) => (
-          <li key={index}>{country}</li>
+          <li key={index}>
+            <button className="countries-button">{country}</button>
+          </li>
         ))}
       </ul>
+
       </body>
     </div>
   );
