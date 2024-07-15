2400dc5b79dd8c74191535927720b64b50461ef0
Amanda Hausmann <anhausma@uci.edu>
2024-07-14 18:42:50 -0700

    code organization + var renaming

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
 import mercatorImage from './images/mercator.jpg';
 
+
 function App() {
 
   const countries = ['Cambodia', 'France', 'Germany', 'Vietnam'];
 
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="UserName-Travels">
+    <div className="dashboard">
+      <header className="dashboard-header">
+        <p className="user-travels">
             Sphynx's Travels
         </p>
-        <img src={mercatorImage} alt="Map" className="App-mercator" />
       </header>
-      <body className="App-body">
-      <h2 className="countries-title">Countries</h2> {}
+      <body className="dashboard-body">
+      <img src={mercatorImage} alt="Map" className="mercator-pic" />
+      <h2 className="countries-title"> Countries </h2>
       <ul className="country-list">
         {countries.map((country, index) => (
           <li key={index}>{country}</li>
