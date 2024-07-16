78c08bc301fa3b026475a22b0747791ae8fa4272
Amanda Hausmann <anhausma@uci.edu>
2024-07-15 21:35:02 -0700

    map with higlighted countries corresponding to countries in list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,29 @@
 import './App.css';
-import mercatorImage from './images/mercator.jpg';
-import React, {useState} from 'react';
+import React from 'react';
+import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
+import {useState} from 'react';
 
+const geoUrl = "https://unpkg.com/world-atlas@2.0.2/countries-110m.json"
 
 function App() {
 
-  const countries = ['Austria', 'Cambodia', 'France', 'Germany', 'Tanzania', 'Vietnam'];
+  const countries = ['Austria', 'Cambodia', 'Canada', 'Costa Rica', 'France', 'Germany', 'Mexico', 'Netherlands', 'Tanzania', 'United Kingdom', 'United States of America', 'Vietnam'];
   const cities = ['Beijing', 'London', 'Paris', 'Tokyo'];
   const territories = ['Puerto Rico', 'Hong Kong', 'Macau', 'Zanzibar'];
   const landmarks = ['Angkor Wat', 'Eiffel Tower', 'Great Wall of China', 'Colosseum'];
   const places = {countries, cities, territories, landmarks};
 
   const [currentList, setList] = useState('countries');
+  const [highlightedCountries, setHighlightedCountries] = useState([]);
+
   const listChange = (listName) => {
     setList(listName);
+    if (listName === 'countries') {
+      setHighlightedCountries(places.countries);
+    }
+    else {
+      setHighlightedCountries([]);
+    }
   };
 
   return (
@@ -29,7 +39,22 @@ function App() {
 
       <body className="dashboard-body">
 
-      <img src={mercatorImage} alt="Map" className="mercator-pic" />
+      <div className="map-container">
+      <ComposableMap>
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map((geo) => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  fill={highlightedCountries.includes(geo.properties.name) ? '#405b9b':"#eaedf0"}
+                  stroke="#FFFFFF"
+                />
+              ))
+            }
+          </Geographies>
+        </ComposableMap>
+      </div>
 
       <div className="buttons-container">
           <button className="list-button" onClick={() => listChange('countries')}>Countries</button>
